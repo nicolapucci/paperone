fb4ecca16be1f31dc7b358b1c88a5c142faa3cfb
nicola <cola08.12.99@gmail.com>
2026-02-12 14:14:15 +0100

    generic

--- paperone/youtrack/youTrack.py
+++ paperone/youtrack/youTrack.py
@@ -4,10 +4,16 @@ import requests
 YOUTRACK_TOKEN = os.getenv('YOUTRACK_TOKEN')
 YOUTRACK_URL = os.getenv('YOUTRACK_URL')
 
-def bug_issues():
 
-    fields = ''
-    query= ''
+fields = 'id,idReadable,created,customFields(name,value(name))'
+base_query= 'project; Kalliope Type: Bug'
+
+last_update = None
+
+update_query = f"{base_query} updated: {last_update} .. Now" #to check the type of the timestamp accepted by youtrack
+
+
+def issues(fields,query):
 
     top = 1000
     skip = 0
@@ -17,7 +23,6 @@ def bug_issues():
     issues = []
 
     while refetch:
-        print('abt to fetch')
 
         response = requests.get(
             headers={
@@ -36,12 +41,22 @@ def bug_issues():
 
         issue_data = response.json()
 
-        refetch = False if (skip%top == 3) else True
-        #refetch = False if len(issue_data)<top else True
+        refetch = False if len(issue_data)<top else True #if youtrack returns less items than requested it means he don-t have any more items to return
         
         skip += top
-        issues.append(issue_data)
+        newdata = []
+        for issue in issue_data:
+            newitem = issue.copy()
+            for field in newdata.customFields:
+                name = field['name']
+                value = field['value']['name'] if (value in field and name in field['value']) else None
+                if(name in ['Origine', 'Type']):
+                    newitem[name]=value
+            del newitem.customFields
+            newdata.append(newitem)
 
+        issues.append(newdata)
+    return issues
 
 if __name__ == "__main__":
-    print(bug_issues())
+    print(issues())
